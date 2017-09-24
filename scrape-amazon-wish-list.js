0c959fa5f5ff29cc5c9ae7e0ea7d575917aae05e
Micah Stubbs <micah.stubbs@gmail.com>
2017-09-24 09:11:19 -0700

    add working amazon wish list scraper

---
+++ scrape-amazon-wish-list.js
@@ -0,0 +1,34 @@
+const jsonfile = require('jsonfile');
+const AmazonListScraper = require('amazon-list-scraper').default;
+
+const listTitle = 'test-list';
+const wishList = 'https://www.amazon.com/gp/registry/wishlist/1JMCNHNT959X2';
+
+const scraper = new AmazonListScraper();
+scraper
+  .scrape(wishList)
+  .then(items => {
+    console.log(items);
+    //  [
+    //    {
+    //      title: 'The Principles of Object-Oriented JavaScript',
+    //      price: 9.99,
+    //      link: 'https://www.amazon.com/dp/B00I87B1H8/ref=wl_it_dp_v_nS_ttl/184-4221310-4664445?_encoding=UTF8&colid=1JMCNHNT959X2&coliid=I2ETH645CXBEGM'
+    //    },
+    //    {
+    //      title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+    //      price: 38.6,
+    //      link: 'https://www.amazon.com/dp/0132350882/ref=wl_it_dp_v_nS_ttl/184-4221310-4664445?_encoding=UTF8&colid=1JMCNHNT959X2&coliid=IDGP10KBLGRPV'
+    //    }
+    //  ]
+    //
+    // write the result out to a json file
+    const outputData = items;
+    const outputDir = 'data';
+    const outputFile = `${outputDir}/${listTitle}.json`;
+    jsonfile.spaces = 2;
+    jsonfile.writeFile(outputFile, outputData, function(err) {
+      console.error(err);
+    });
+  })
+  .catch(error => {});
